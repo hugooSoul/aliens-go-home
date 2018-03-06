7ef60d9aa85e9ca15a7687a57d877eaf05e6e1f0
Hugo López Mandujano <hugol.soul@gmail.com>
2018-03-06 14:37:58 -0600

    creating the flying objects and the hearts that going to represent lifes

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -5,6 +5,9 @@ import Ground from './Ground';
 import CannonBase from './CannonBase';
 import CannonPipe from './CannonPipe';
 import CannonBall from './CannonBall';
+import CurrentScore from './CurrentScore';
+import FlyingObject from './FlyingObject';
+import Heart from './Heart';
 
 const Canvas = (props) => {
 
@@ -20,11 +23,20 @@ const Canvas = (props) => {
     preserveAspectRatio="xMaxYMax none"
     onMouseMove={props.trackMouse}
     viewBox={viewBox} >
+      <defs>
+        <filter id="shadow">
+          <feDropShadow dx="1" dy="1" stdDeviation="2" />
+        </filter>
+      </defs>
       <Sky />
       <Ground />
       <CannonPipe rotation={props.angle} />
       <CannonBase />
       <CannonBall position={{x: 0, y: -100}}/>
+      <CurrentScore score={15} />
+      <FlyingObject position={{x: -150, y: -400}}/>
+      <FlyingObject position={{x: 150, y: -400}}/>
+      <Heart position={{x: -300, y: 35}} />
     </svg>
   );
 
