c9502457bf2ea8da68c53e67e062e20a3081d541
Hugo López Mandujano <hugol.soul@gmail.com>
2018-03-06 13:27:58 -0600

    defining the angle on the initialState, and using onMouseMove to track the mouse position and return a new angle and display it making the cannon moves

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Sky from './Sky';
 import Ground from './Ground';
 import CannonBase from './CannonBase';
 import CannonPipe from './CannonPipe';
 
-const Canvas = () => {
+const Canvas = (props) => {
 
   const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
 
@@ -16,14 +17,20 @@ const Canvas = () => {
     <svg
     id="aliens-go-home-canvas"
     preserveAspectRatio="xMaxYMax none"
+    onMouseMove={props.trackMouse}
     viewBox={viewBox} >
       <Sky />
       <Ground />
-      <CannonPipe rotation={45} />
+      <CannonPipe rotation={props.angle} />
       <CannonBase />
     </svg>
   );
 
+};
+
+Canvas.propTypes = {
+  angle: PropTypes.number.isRequired,
+  trackMouse: PropTypes.func.isRequired,
 };
 
 export default Canvas;
