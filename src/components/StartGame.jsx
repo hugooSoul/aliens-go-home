cbf08bcd798cf48fa36187205c27aff3a1815133
Hugo López Mandujano <hugol.soul@gmail.com>
2018-03-07 20:52:41 -0600

    Crating a Login button connecting to Auth0 and a leaderboard component

--- src/components/StartGame.jsx
+++ src/components/StartGame.jsx
@@ -20,7 +20,7 @@ const StartGame = (props) => {
   const text = {
     textAnchor: 'middle', // center
     x: 0, // center relative to X axis
-    y: -350, // 150 up
+    y: -150, // 150 up
     style: {
       fontFamily: 'Poppins', //'"Joti One", cursive'
       fontSize: 60,
